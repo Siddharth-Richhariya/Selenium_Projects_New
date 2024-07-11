6cf814dcab58eb656be70edc7e40f3acab218f6c
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-07-12 02:14:51 +0530

    New

--- src/test/java/org/example/BAse.java
+++ src/test/java/org/example/BAse.java
@@ -1,5 +1,14 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BAse {
-    static
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        driver = new ChromeDriver();
+        driver.get("https://github.com/Siddharth-Richhariya/Selenium_Projects.git");
+        driver.close();
+    }
 }
